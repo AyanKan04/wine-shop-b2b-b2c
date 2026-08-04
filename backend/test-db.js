bc6f58a29a2577c77b9426d21babcd4321b0c69f
Hung Yen <voy32103@gmail.com>
2026-08-04 09:06:42 +0700

    chore(cleanup): remove unnecessary scratch files and update API documentation

--- backend/test-db.js
+++
@@ -1,9 +0,0 @@
-const { getPool } = require('./src/config/db');
-
-async function test() {
-  const pool = await getPool();
-  const res = await pool.request().query('SELECT * FROM Products WHERE ProductID = 8');
-  console.log("Product 8:", res.recordset);
-  process.exit(0);
-}
-test().catch(console.error);
